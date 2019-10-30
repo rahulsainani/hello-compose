642798c0371a7b8abfaa6d449c22e2a1157e9c80
Rahul <rahulsainani@wtinfotech.co.uk>
2019-10-31 00:57:27 +0100

    Default Compose project

---
+++ app/src/main/java/app/droidster/hellocompose/MainActivity.kt
@@ -0,0 +1,28 @@
+package app.droidster.hellocompose
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.Composable
+import androidx.ui.core.Text
+import androidx.ui.core.setContent
+import androidx.ui.tooling.preview.Preview
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            Greeting()
+        }
+    }
+}
+
+@Composable
+fun Greeting() {
+    Text("Hello Compose")
+}
+
+@Preview
+@Composable
+fun DefaultPreview() {
+    Greeting()
+}
